a7089d8f7b940bc050ab492a0082f785a819e597
guy <gtk.gtk5@gmail.com>
2021-06-05 16:49:13 +0300

    removed double spaces

--- src/main/java/frc/team3388/robot/RobotMap.java
+++ src/main/java/frc/team3388/robot/RobotMap.java
@@ -7,10 +7,8 @@ public class RobotMap {
 
     // EXAMPLE SYSTEM
     public static final int EXAMPLE_SYSTEM_MOTOR = 5;
-
     // HID
     public static final HidChannel XBOX = RoboRio.newHidChannel(0);
-
     public static final int INTAKE_SYSTEM_MOTOR = 1;
     public static final int PISTON_REVERSE_CHANNEL = 2;
     public static final int PISTON_FORWARD_CHANNEL = 3;
