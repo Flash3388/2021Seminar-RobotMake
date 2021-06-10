c9136c618639a1e3603af369b95900d056f44bd8
guy <gtk.gtk5@gmail.com>
2021-06-10 16:01:38 +0300

    Food for code

--- src/main/java/frc/team3388/robot/Robot.java
+++ src/main/java/frc/team3388/robot/Robot.java
@@ -2,29 +2,42 @@ package frc.team3388.robot;
 
 import com.flash3388.flashlib.frc.robot.FrcRobotControl;
 import com.flash3388.flashlib.frc.robot.base.iterative.IterativeFrcRobot;
+import com.flash3388.flashlib.hid.XboxButton;
 import com.flash3388.flashlib.hid.XboxController;
 import com.flash3388.flashlib.robot.base.DelegatingRobotControl;
-import frc.team3388.robot.subsystems.ExampleSystem;
-import frc.team3388.robot.subsystems.IntakeSystem;
+import frc.team3388.robot.actions.CollectBalls;
+import frc.team3388.robot.actions.FoldIntake;
+import frc.team3388.robot.actions.MovePistons;
+import frc.team3388.robot.actions.UnfoldIntake;
+import frc.team3388.robot.subsystems.*;
 
 public class Robot extends DelegatingRobotControl implements IterativeFrcRobot {
 
-    private final ExampleSystem exampleSystem;
     private final IntakeSystem intakeSystem;
+    private final HopperSystem hopperSystem;
+    private final ShooterSystem shooterSystem;
+    private final DriveSystem driveSystem;
 
     private final XboxController xbox;
+
+    private final FeederSystem feederSystem;
 
     public Robot(FrcRobotControl robotControl) {
         super(robotControl);
 
         // CREATE SUBSYSTEMS
         SystemFactory systemFactory = new SystemFactory(robotControl);
-        exampleSystem = systemFactory.createExampleSystem();
         intakeSystem = systemFactory.createIntakeSystem();
+        hopperSystem = systemFactory.createHopperSystem();
+        shooterSystem=systemFactory.createShooterSystem();
+        driveSystem = systemFactory.createDriveSystem();
+        feederSystem = systemFactory.createfeedersystem();
         // CREATE CONTROLLERS
         xbox = getHidInterface().newXboxController(RobotMap.XBOX);
 
         // CONFIGURE ACTIONS
+        xbox.getDpad().down().whenActive(new MovePistons(intakeSystem));
+        xbox.getButton(XboxButton.LB).whileActive(new CollectBalls(intakeSystem));
     }
 
     @Override
