9e74cff1e835cb49adda5ecc43a9f7d1e8d461a4
Maayan <maayan.agam.alon@gmail.com>
2021-06-10 16:08:50 +0300

    Shooter System

--- src/main/java/frc/team3388/robot/actions/ShootBall.java
+++ src/main/java/frc/team3388/robot/actions/ShootBall.java
@@ -1,6 +1,5 @@
 package frc.team3388.robot.actions;
 
-import com.flash3388.flashlib.hid.XboxButton;
 import com.flash3388.flashlib.hid.XboxController;
 import com.flash3388.flashlib.scheduling.actions.ActionBase;
 import frc.team3388.robot.subsystems.ShooterSystem;
@@ -9,11 +8,13 @@ public class ShootBall extends ActionBase {
 
     private final ShooterSystem shootSystem;
     private final XboxController shooterXboxController;
+    private final double speed;
 
 
-    public ShootBall(ShooterSystem shootSystem, XboxController shooterXboxController) {
+    public ShootBall(ShooterSystem shootSystem, XboxController shooterXboxController, double speed) {
         this.shootSystem = shootSystem;
         this.shooterXboxController = shooterXboxController;
+        this.speed = speed;
 
         requires(shootSystem);
     }
@@ -26,10 +27,19 @@ public class ShootBall extends ActionBase {
 
     @Override
     public void execute() {
-        boolean buttonShooter= shooterXboxController.getButton(XboxButton.RB).getAsBoolean();
-        while(buttonShooter==true) {
+       // double absSpeed=Math.abs(speed);
+        if(speed>0&&speed<0.2){
+            shootSystem.stop();
+        }
+        shootSystem.shoot(speed);
+
+        /*  boolean buttonShooter= shooterXboxController.getButton(XboxButton.RB).getAsBoolean();
+        if(buttonShooter==true) {
             shootSystem.shoot(0.5);
         }
+        else{
+        shootSystem.stop();
+        }*/
     }
 
     @Override
